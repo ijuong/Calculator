b7d21f9cdfbf755855cf5906d147ff3df27e567c
Juyong Lee <juyong@sangwooui-MacBook-Air.local>
2018-09-26 02:33:25 +0900

    180926

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -11,35 +11,90 @@ import Foundation
 class CalculatorBrain {
     
     private var accumulator = 0.0
+    private var internalProgram = [AnyObject]()
     
     func setOperand(operand: Double) {
         accumulator = operand
+        internalProgram.append(operand as AnyObject)
     }
     
-    var operations: Dictionary<String, Operation> = [
+    private var operations: Dictionary<String, Operation> = [
         "π" : Operation.Constant(M_PI),
         "e" : Operation.Constant(M_E),
         "√" : Operation.UnaryOperation(sqrt),
-        "cos" : Operation.UnaryOperation(cos)
+        "cos" : Operation.UnaryOperation(cos),
+        "±" : Operation.UnaryOperation({ -$0 }),
+        "×" : Operation.BinaryOperation({ $0 * $1 }),
+        "÷" : Operation.BinaryOperation({ $0 / $1 }),
+        "+" : Operation.BinaryOperation({ $0 + $1 }),
+        "−" : Operation.BinaryOperation({ $0 - $1 }),
+        "=" : Operation.Equals
     ]
     
-    enum Operation {
+    private enum Operation {
         case Constant(Double)
         case UnaryOperation((Double) -> Double)
-        case BinaryOperation
+        case BinaryOperation((Double, Double) -> Double)
         case Equals
     }
     
     func performOperation(symbol : String) {
+        internalProgram.append(symbol as AnyObject)
         
         if let operation = operations[symbol] {
             switch operation {
-            case .Constant(let value) : accumulator = value
-            case .UnaryOperation(let function) : accumulator = function(accumulator)
-            case .BinaryOperation : break
-            case .Equals : break
+            case .Constant(let value) :
+                accumulator = value
+            case .UnaryOperation(let function) :
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function) :
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals :
+                executePendingBinaryOperation()
             }
         }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending : PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction : (Double, Double) -> Double
+        var firstOperand : Double
+    }
+    
+    typealias PropertyList = AnyObject
+    
+    var program : PropertyList {
+        get {
+            return internalProgram as CalculatorBrain.PropertyList
+        }
+        set {
+            clear()
+            
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    } else if let operation = op as? String {
+                        performOperation(symbol: operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    func clear() {
+        accumulator = 0.0
+        pending = nil
+        internalProgram.removeAll()
     }
     
     var result : Double {
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet private weak var display: UILabel!
+    @IBOutlet private weak var history: UILabel!
     
     private var userIsInTheMiddleOfTyping = false
+    private var userIsCalculatorHistory = false;
     
     @IBAction private func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
+        
+        if digit == "." && (display.text?.contains("."))! {
+            return
+        }
+        
         if userIsInTheMiddleOfTyping {
             let textCurrentlyInDisplay = display.text! 
             display.text = textCurrentlyInDisplay + digit
@@ -24,7 +31,8 @@ class ViewController: UIViewController {
         }
         
         userIsInTheMiddleOfTyping = true
-        
+
+        displayHistory(value: digit)
     }
     
     private var displayValue : Double {
@@ -36,10 +44,63 @@ class ViewController: UIViewController {
         }
     }
     
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant,
+        "e" : Operation.Constant,
+        "√" : Operation.UnaryOperation,
+        "cos" : Operation.UnaryOperation,
+        "=" : Operation.Equals
+    ]
+    
+    private enum Operation {
+        case Constant
+        case UnaryOperation
+        case Equals
+    }
+
+    private func displayHistory(value: String) {
+        
+        let historyRecord = history.text!
+        let displayValue = display.text!
+        
+        if let operation = operations[value] {
+            switch operation {
+            case .Constant :
+                history.text = value + "=" + displayValue
+            case .UnaryOperation :
+                history.text = historyRecord + value + "=" + displayValue
+            case .Equals :
+                history.text = historyRecord + value + displayValue
+            }
+            userIsCalculatorHistory = false
+        }else{
+            if !userIsCalculatorHistory {
+                history.text = value
+                userIsCalculatorHistory = true
+            }else{
+                history.text = historyRecord + value
+            }
+        }
+    }
+    
     var brain : CalculatorBrain = CalculatorBrain()
     
+    var savedProgram : CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
     @IBAction private func performOperation(_ sender: UIButton) {
-        
+        let operand = sender.currentTitle!;
+
         if userIsInTheMiddleOfTyping {
             brain.setOperand(operand: displayValue)
             userIsInTheMiddleOfTyping = false
@@ -50,6 +111,11 @@ class ViewController: UIViewController {
         }
         
         displayValue = brain.result
+
+        if !userIsCalculatorHistory {
+            userIsCalculatorHistory = true
+        }
+        displayHistory(value: operand)
     }
 }
 
